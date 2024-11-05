c4252fa5a7a485d95255f52832c697e1a15666b1
Gitudon <oniku.udon.029@gmail.com>
2024-11-05 11:17:49 +0900

    依存関係の解消

--- UDC_Announce/bot/main.py
+++ UDC_Announce/bot/main.py
@@ -12,52 +12,33 @@ client = commands.Bot(
     command_prefix='!',
     intents=intent
 )
-
-days = [3,1,0,0,0,0,2]
+#月曜が0,日曜が6
+today =  [1,0,0,0,0,0,0,0,1,0,0,0,0,0]
+tommorow=[0,0,0,0,0,0,0,1,0,0,0,0,0,1]
 channel_id = int(os.environ.get("CHANNEL_ID"))
 test_channel_id = int(os.environ.get("TEST_CHANNEL_ID"))
-tf=False
+channel = client.get_channel(channel_id)
+test_channel = client.get_channel(test_channel_id)
+plus=7 #起動時に決める(0/7)
 
-@client.event
-async def on_message(message):
-    if message.content == "!test":
-        if message.channel.id == channel_id or message.channel.id == test_channel_id:
-            await message.channel.send(f"Announcement Bot is Working!\nstatus: {tf}")
-    return
+@client.command
+async def test(ctx):
+    if ctx.id == channel_id or ctx.id == test_channel_id:
+        await ctx.send("Announcement Bot is Working!")
 
-async def announce(done):
-    channel = client.get_channel(channel_id)
-    if done:
-        message="@everyone\n今日は定例会です！"
-    else:
-        message="@everyone\n明日は定例会です！"
-    if channel:
-        await channel.send(message)
-    return
+async def announce_tommorow():
+    await channel.send("@everyone\n明日は定例会です！")
 
-async def testannounce_morning(done):
-    channel = client.get_channel(test_channel_id)
-    if done:
-        message="True"
-    else:
-        message="False"
-    if channel:
-        await channel.send("定時連絡(朝)\n"+message)
-    return
+async def announce_today():
+    await channel.send("@everyone\n今日は定例会です！")
 
-async def testannounce_evening(done):
-    channel = client.get_channel(test_channel_id)
-    if done:
-        message="True"
-    else:
-        message="False"
-    if channel:
-        await channel.send("定時連絡(夕方)\n"+message)
-    return
+async def testannounce_morning():
+    await test_channel.send("定時連絡(朝)")
+
+async def testannounce_evening():
+    await test_channel.send("定時連絡(夜)")
 
 async def check_time():
-    global tf
-    done=False
     while True:
         # Morning check at 6 AM
         now = datetime.datetime.now()
@@ -67,30 +48,20 @@ async def check_time():
         seconds_until = (next_morning - now).total_seconds()
         await asyncio.sleep(seconds_until)
         current_time = datetime.datetime.now()
-        day_of_week = current_time.weekday()
-        if days[day_of_week] in [1, 3]:
-            if done:
-                if days[day_of_week] == 3:
-                    done = False
-            else:
-                await announce(done)
-                if days[day_of_week] == 3:
-                    done = True
-        await testannounce_morning(done)
-        tf=done
+        day_of_week = (current_time.weekday()+plus)
+        if today[day_of_week] == 1:
+            await announce_today()
+        await testannounce_morning()
         # Evening check at 6 PM
         now = datetime.datetime.now()
         next_evening = now.replace(hour=18, minute=0, second=0, microsecond=0)
-        if now >= next_evening:
-            next_evening += datetime.timedelta(days=1)
         seconds_until = (next_evening - now).total_seconds()
         await asyncio.sleep(seconds_until)
         current_time = datetime.datetime.now()
-        day_of_week = current_time.weekday()
-        if days[day_of_week] in [2, 3]:
-            if not done:
-                await announce(done)
-        await testannounce_evening(done)
+        day_of_week = (current_time.weekday()+plus)
+        if tommorow[day_of_week] == 1:
+            await announce_tommorow()
+        await testannounce_evening()
 
 @client.event
 async def on_ready():
