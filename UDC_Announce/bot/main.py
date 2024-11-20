38b854043d8f93fa8fbd47e3734795005b1ce872
Gitudon <oniku.udon.029@gmail.com>
2024-11-20 20:58:56 +0900

    記述ミスの修正

--- UDC_Announce/bot/main.py
+++ UDC_Announce/bot/main.py
@@ -31,29 +31,32 @@ async def announce_today():
     channel = client.get_channel(channel_id)
     await channel.send("@everyone\n今日は定例会です！")
 
-async def testannounce_morning():
+async def testannounce_morning(day_of_week):
     test_channel = client.get_channel(test_channel_id)
-    await test_channel.send("定時連絡(朝)")
+    await test_channel.send(f"定時連絡(朝) {day_of_week}")
 
-async def testannounce_evening():
+async def testannounce_evening(day_of_week):
     test_channel = client.get_channel(test_channel_id)
-    await test_channel.send("定時連絡(夜)")
+    await test_channel.send(f"定時連絡(夜) {day_of_week}")
 
 async def check_time():
-    plus=0 #起動時に決める(0/7)
+    plus=7 #起動時に決める(0/7)
     while True:
         # Morning check at 6 AM
         now = datetime.datetime.now()
         next_morning = now.replace(hour=6, minute=0, second=0, microsecond=0)
-        if now >= next_morning:
-            next_morning += datetime.timedelta(days=1)
-        seconds_until = (next_morning - now).total_seconds()
-        await asyncio.sleep(seconds_until)
-        current_time = datetime.datetime.now()
-        day_of_week = (current_time.weekday()+plus)
-        if today[day_of_week] == 1:
-            await announce_today()
-        await testannounce_morning()
+        if 6<=now.hour<18:
+            pass
+        else:
+            if 18<=now.hour<=23:
+                next_morning += datetime.timedelta(days=1)
+            seconds_until = (next_morning - now).total_seconds()
+            await asyncio.sleep(seconds_until)
+            current_time = datetime.datetime.now()
+            day_of_week = (current_time.weekday()+plus)
+            if today[day_of_week] == 1:
+                await announce_today()
+            await testannounce_morning(day_of_week)
         # Evening check at 6 PM
         now = datetime.datetime.now()
         next_evening = now.replace(hour=18, minute=0, second=0, microsecond=0)
@@ -63,7 +66,7 @@ async def check_time():
         day_of_week = (current_time.weekday()+plus)
         if tommorow[day_of_week] == 1:
             await announce_tommorow()
-        await testannounce_evening()
+        await testannounce_evening(day_of_week)
         if day_of_week==13:
             plus=0
         elif day_of_week==6:
