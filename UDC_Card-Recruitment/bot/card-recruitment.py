7b8def46aec5d3908a2b3b7ef5ffdfdbcb77a50d
Gitudon <oniku.udon.029@gmail.com>
2025-03-08 10:06:26 +0900

    取引機能をオミット

--- UDC_Card-Recruitment/bot/card-recruitment.py
+++ UDC_Card-Recruitment/bot/card-recruitment.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-from save import *
+from save_list import *
 
 # 初期設定
 TOKEN = os.getenv("TOKEN")
@@ -30,18 +30,12 @@ async def guide(ctx):
             "-guide\n"
             "【募集追加】\n"
             "-want [カード名] [枚数]\n"
+            "※既存のカード名を指定した場合枚数が更新されます。\n"
             "【募集確認】\n"
             "-check [カード名/人](個別確認)\n"
             "-check (引数なしで全体確認)\n"
             "【募集終了】\n"
             "-end [カード名]\n"
-            "※募集の更新は「一度募集を終了して再度募集を行う」形でお願いします。\n"
-            "【取引追加】(募集した人が合意後に実行する)\n"
-            "-trade [取引相手] [カード名] [枚数]\n"
-            "【取引完了】\n"
-            "-done [取引番号]\n"
-            "【進行中の取引一覧】\n"
-            "-list\n"
             "```"
         )
 @client.command()
@@ -60,14 +54,21 @@ async def want(ctx, *args):
             if num.isdecimal():
                 num = int(num)
                 if person in recruitment:
-                    flag=True
                     for i in range(len(recruitment[person])):
                         if recruitment[person][i]["want"] == want:
-                            recruitment[person][i]["num"] += num
+                            buf = recruitment[person][i]["num"]
+                            recruitment[person][i]["num"] = num
+                            state = recruitment[person][i]["active"]
                             recruitment[person][i]["active"] = True
-                            flag=False
-                    if flag:
-                        recruitment[person].append({"want":want, "num":num, "active":True})
+                            if state:
+                                if buf != num:
+                                    await ctx.send(f"{person}さんの募集を更新しました：\n{want} ×{buf} → ×{num}")
+                                else:
+                                    await ctx.send(f"{person}さんはすでにその内容の募集を行っています。")
+                            else:
+                                ctx.send(f"{person}さんの募集を受け付けました：\n{want} ×{num}")
+                            return
+                    recruitment[person].append({"want":want, "num":num, "active":True})
                 else:
                     recruitment[person] = [{"want":want, "num":num, "active":True}]
                 await ctx.send(f"{person}さんの募集を受け付けました：\n{want} ×{num}")
@@ -161,81 +162,6 @@ async def end(ctx, *args):
         else:
             await ctx.send("募集終了方法に誤りがあります。")
 @client.command()
-async def trade(ctx, *args):
-    if await check_channel(ctx):
-        if len(args) == 3:
-            transaction_number = str(len(transaction)+1)
-            person_from = args[0]
-            person_to = ctx.author.display_name
-            card = args[1]
-            if args[2].isdecimal():
-                num = int(args[2])
-                if person_to in recruitment:
-                    flag=False
-                    for i in range(len(recruitment[person_to])):
-                        foo = recruitment[person_to][i]
-                        if foo["want"] == card:
-                            if foo["active"]:
-                                flag=True
-                                if recruitment[person_to][i]["num"] >= num:
-                                    break
-                                else:
-                                    foo = recruitment[person_to][i]["num"]
-                                    await ctx.send(f"{person_to}さんが募集している枚数({foo})を超えています。")
-                                    return
-                            else:
-                                await ctx.send(f"{person_to}さんは「{card}」 の募集をすでに締め切っています。")
-                                return
-                    if flag:
-                        transaction[transaction_number] = {"person-from":person_from, "person-to":person_to, "card":card, "num":num, "active":True}
-                        await ctx.send(f"「取引{transaction_number}」が追加されました：\n{person_from} → {person_to} - {card} ×{num}")
-                    else:
-                        await ctx.send(f"{person_to}さんは「{card}」 を募集していません。")
-                else:
-                    await ctx.send(f"{person_to}さんが募集しているカードはありません。")
-            return
-        await ctx.send("取引追加方法に誤りがあります。")
-@client.command()
-async def done(ctx, *args):
-    if await check_channel(ctx):
-        if len(args) == 1:
-            if args[0] in transaction:
-                foo = transaction[args[0]]
-                if foo["active"]:
-                    foo["active"] = False
-                    bar = recruitment[foo["person-from"]]
-                    for i in range(len(bar)):
-                        if bar[i]["want"] == foo["card"]:
-                            baz = bar[i]["num"]
-                            baz -= foo["num"]
-                            if baz <= 0:
-                                baz = 0
-                                recruitment[foo["person-from"]][i]["active"] = False
-                            recruitment[foo["person-from"]][i]["num"] = baz
-                    await ctx.send(f"取引{args[0]}が完了しました：\n{foo['person-from']} → {foo['person-to']} - {foo['card']} ×{foo['num']}")
-                else:
-                    await ctx.send("既に完了している取引です。")
-            else:
-                await ctx.send("該当する取引が存在しません。")
-        else:
-            await ctx.send("取引完了方法に誤りがあります。")
-@client.command()
-async def list(ctx):
-    if await check_channel(ctx):
-        text=""
-        active_transaction_number = 0
-        for key in transaction:
-            if transaction[key]["active"]:
-                active_transaction_number += 1
-        if active_transaction_number == 0:
-            text = "進行中の取引はありません。"
-        else:
-            for key in transaction:
-                if transaction[key]["active"]:
-                    text += f"取引{key}：{transaction[key]['person-from']} → {transaction[key]['person-to']} - {transaction[key]['card']} ×{transaction[key]['num']}\n"
-            text = text[:-1]
-        await ctx.send(text)
-@client.command()
 async def test(ctx):
     if await check_channel(ctx):
         await ctx.send("Card-Recruitment Bot is Working!")
@@ -244,7 +170,6 @@ async def save(ctx):
     if await check_channel(ctx):
         await ctx.send("現時点での内容をセーブしました。")
         print(recruitment)
-        print(transaction)
 @client.event
 async def on_ready():
     print("Bot is ready!")
--- UDC_Card-Recruitment/bot/save.py
+++
@@ -1,5 +0,0 @@
-# データ構造：既存のデータがあるなら起動前にいじっておく
-recruitment = {}
-# {person: [{want:str, num:int, active:bool}]}
-transaction = {}
-# {transaction-number: {person-from:str, person-to:str, card:str, num:int, active:bool}}
